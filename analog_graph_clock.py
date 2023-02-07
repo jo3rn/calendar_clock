c84db5291241524f0b04de9405c719413a556689
Pascal Gutermuth <pascal.gutermuth@informatik.hs-fulda.de>
2023-02-07 20:47:52 +0100

    Fenster hinzufuegen

---
+++ analog_graph_clock.py
@@ -0,0 +1,9 @@
+import turtle
+
+window = turtle.Screen()
+window.bgcolor(0.1, 0.1, 0.1)
+window.setup(600, 600)
+window.title("Graphic representation of a clock")
+
+if __name__ == "__main__":
+    window.mainloop()
